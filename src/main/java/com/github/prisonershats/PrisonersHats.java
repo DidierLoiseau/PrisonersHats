4460736e5679c66fd9d292da086f74f494e8bc56
Didier Loiseau <didierloiseau+git@gmail.com>
2016-04-10 22:16:13 +0200

    Implemented command-line options

--- src/main/java/com/github/prisonershats/PrisonersHats.java
+++ src/main/java/com/github/prisonershats/PrisonersHats.java
@@ -1,18 +1,68 @@
 package com.github.prisonershats;
 
+import static java.util.Arrays.asList;
+
 import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.prisonershats.checkers.SimpleHatsChecker;
 import com.github.prisonershats.generators.IntegerHatsGenerator;
 import com.github.prisonershats.strategies.MeanBasedStrategy;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
 public class PrisonersHats {
+	private static final Logger LOG = LoggerFactory.getLogger(PrisonersHats.class);
+
+	private static final OptionParser PARSER = new OptionParser();
+	private static final OptionSpec<Integer> PRISONERS = PARSER
+			.acceptsAll(asList("p", "prisoners"), "number of prisoners")
+			.withRequiredArg().ofType(Integer.class)
+			.defaultsTo(100);
+	private static final OptionSpec<Integer> TESTS = PARSER
+			.acceptsAll(asList("t", "tests"), "number of tests")
+			.withRequiredArg().ofType(Integer.class)
+			.defaultsTo(1000);
+	private static final OptionSpec<String> STRATEGY = PARSER
+			.acceptsAll(asList("s", "strategy"), "the strategy to use")
+			.withRequiredArg();
+	private static final OptionSpec<Long> SEED = PARSER
+			.acceptsAll(asList("r", "seed"), "random seed")
+			.withRequiredArg().ofType(Long.class);
+	static {
+		PARSER.acceptsAll(asList("?", "h", "help"), "shows this help").forHelp();
+	}
 
 	public static void main(String[] args) throws Exception {
-		int n = 100;
-		int testsCount = 1000;
-		PrisonersHatsStrategy<Integer> solver = new MeanBasedStrategy();
-		Random random = new Random(/* 42 */);
+		OptionSet options = PARSER.parse(args);
+		if (options.has("help")) {
+			PARSER.printHelpOn(System.out);
+			return;
+		}
+
+		int n = options.valueOf(PRISONERS);
+		int testsCount = options.valueOf(TESTS);
+		// TODO add some type checking
+		PrisonersHatsStrategy<Integer> solver;
+		if (options.has(STRATEGY)) {
+			Class<?> strategyClass = Class
+					.forName(MeanBasedStrategy.class.getPackage().getName() + "." + options.valueOf(STRATEGY));
+			solver = strategyClass.asSubclass(PrisonersHatsStrategy.class).newInstance();
+		} else {
+			solver = new MeanBasedStrategy();
+		}
+		LOG.info("Using strategy: {}", solver.getClass().getSimpleName());
+		Random random;
+		if (options.has(SEED)) {
+			random = new Random(options.valueOf(SEED));
+		} else {
+			random = new Random(/* 42 */);
+		}
+
 		HatsGenerator<Integer> generator = new IntegerHatsGenerator(random);
 		HatsChecker<Integer> checker = new SimpleHatsChecker<>();
 
