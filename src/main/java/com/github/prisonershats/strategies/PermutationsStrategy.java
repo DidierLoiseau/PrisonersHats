051aecbc08fac5538e355693b2f4072ede760aaa
Didier Loiseau <didierloiseau+git@gmail.com>
2016-05-22 22:42:33 +0200

    PermutationStrategy: code cleanup

--- src/main/java/com/github/prisonershats/strategies/PermutationsStrategy.java
+++ src/main/java/com/github/prisonershats/strategies/PermutationsStrategy.java
@@ -1,51 +1,51 @@
 package com.github.prisonershats.strategies;
 
-import com.github.prisonershats.PrisonersHatsStrategy;
+import static java.util.function.Function.identity;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import com.github.prisonershats.PrisonersHatsStrategy;
 
 public class PermutationsStrategy implements PrisonersHatsStrategy<Integer> {
 
-    public Integer guessHat(List<Integer> heardBefore, List<Integer> seenAhead) {
-        // -1 = ?
-        // -2 = X
-        int[] full = new int[heardBefore.size() + seenAhead.size() + 2];
-        for (int i = 0; i < heardBefore.size(); i++) {
-            full[i] = heardBefore.get(i);
-        }
-        full[heardBefore.size()] = -1;
-        for (int i = 0; i < seenAhead.size(); i++) {
-            full[heardBefore.size() + 1 + i] = seenAhead.get(i);
-        }
-        full[heardBefore.size() + 1 + seenAhead.size()] = -2;
+	@Override
+	public Integer guessHat(List<Integer> heardBefore, List<Integer> seenAhead) {
+		// initially:
+		// my position = -1 (unknown hat we need to guess)
+		// fake last position = -2 (the other unknown hat)
+		int[] full = Stream.of(heardBefore.stream(), Stream.of(-1),
+				seenAhead.stream(), Stream.of(-2))
+				.flatMap(identity())
+				.mapToInt(i -> i)
+				.toArray();
 
-        int permutationCount = 0;
-        for (int i = 0; i < full.length; i++) {
-            while (full[i] != -1 && full[i] != -2 && full[i] != i) {
-                int temp = full[full[i]]; // -2
-                full[full[i]] = full[i];
-                full[i] = temp;
-                permutationCount++;
-            }
-        }
-        if (permutationCount%2 == 0) {
-            // find index where -2
-            for (int i = 0; i < full.length; i++) {
-                if (full[i] == -2) {
-                    return i;
-                }
-            }
-        }
-        else {
-            // find index where -1
-            for (int i = 0; i < full.length; i++) {
-                if (full[i] == -1) {
-                    return i;
-                }
-            }
-        }
-
-        return 1;
-    }
+		int permutationCount = 0;
+		int minusOnePos = heardBefore.size();
+		int minusTwoPos = full.length - 1;
+		for (int i = 0; i < full.length; i++) {
+			while (full[i] != -1 && full[i] != -2 && full[i] != i) {
+				int temp = full[full[i]];
+				full[full[i]] = full[i];
+				full[i] = temp;
+				permutationCount++;
+			}
+			if (full[i] == -1) {
+				minusOnePos = i;
+			} else if (full[i] == -2) {
+				minusTwoPos = i;
+			}
+		}
+		if (permutationCount % 2 == 0) {
+			// keep an even parity:
+			// replace -1 by the position it ended at, no more swap required.
+			return minusOnePos;
+		} else {
+			// force an even parity:
+			// replace -1 by the position at which -2 ended, so they would need
+			// one more swap to make the parity even.
+			return minusTwoPos;
+		}
+	}
 
 }
